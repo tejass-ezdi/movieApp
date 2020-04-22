93eae63bfc09070ea4900566c641c89f4d348983
tejass-ezdi <tejas14883@gmail.com>
2020-04-22 11:57:30 +0530

    Added Pako plugin for compress request

--- src/app/services/moveie.service.ts
+++ src/app/services/moveie.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, EMPTY } from 'rxjs';
+import { map, tap, catchError } from 'rxjs/operators';
+import { error } from 'protractor';
+import { inflate, deflate, gzip } from 'pako';
 
 export enum SearchType {
   all = '',
@@ -16,13 +18,50 @@ export class MoveieService {
   url = 'http://www.omdbapi.com/';
   apiKey = '2be398c0'
 
-  constructor( private httpClient: HttpClient) { }
+  constructor( private httpClient: HttpClient ) { }
+
+  getSpecialHeaders() {
+    return new HttpHeaders({
+      'Accept-Encoding': 'gzip, deflate'
+    });
+}
+
 
   searchData(title: string, type: SearchType): Observable<any> {
-    return this.httpClient.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apiKey=${this.apiKey}`)
+    const headersSpl = this.getSpecialHeaders();
+    const httpHeaders = {
+      headers : headersSpl
+    };
+    
+    let reqParam = {
+      s:encodeURI(title),
+      type:type,
+      apiKey:this.apiKey
+    }
+    console.log("Original reqparam ", reqParam);
+
+    let binaryString = deflate(JSON.stringify(reqParam), {to : 'string'});
+    console.log("deflate reqparam:: binary string ", binaryString );
+
+    let gzipReq = gzip(JSON.stringify(reqParam));
+    console.log("GZIP ", gzipReq);
+
+    let restored = JSON.parse(inflate(binaryString, { to: 'string' }));
+    console.log("inflateReq ", restored);
+
+    
+
+    const httpOptions = {
+      params: reqParam,
+      
+    };
+  
+    return this.httpClient.get(`${this.url}`,httpOptions )
     .pipe(
-      map( results => results['Search'])
-    )
+      map( results => results['Search']),
+      tap(console),
+      catchError( error => { console.log(error); return EMPTY })
+    );
   }
 
   getDetails(id) {
